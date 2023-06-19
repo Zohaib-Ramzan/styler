6be4307a07748af3abdb0ed1898e847f5fcaad0b
unknown <zohaibramzan711@gmail.com>
2023-06-19 14:23:49 +0500

    initial Commit

---
+++ components/FlatCards.tsx
@@ -0,0 +1,56 @@
+import {SafeAreaView, StyleSheet, Text, View, FlatList} from 'react-native';
+import React from 'react';
+
+export default function FlatCards() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Flat Card</Text>
+
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+        <View style={[styles.card, styles.cardTwo]}>
+          <Text>Green</Text>
+        </View>
+        <View style={[styles.card, styles.cardThree]}>
+          <Text>Blue</Text>
+        </View>
+        <View style={[styles.card, styles.cardOne]}>
+          <Text>Red</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 8,
+  },
+  card: {
+    // flex : 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 100,
+    height: 100,
+    borderRadius: 4,
+    margin: 8,
+  },
+  cardOne: {
+    backgroundColor: '#FF3131',
+  },
+  cardTwo: {
+    backgroundColor: '#39FF11',
+  },
+  cardThree: {
+    backgroundColor: '#1F51FF',
+  },
+});
